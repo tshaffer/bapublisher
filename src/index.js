ed81d3fa41e1d876c4d458529669a65fb2398c55
Ted Shaffer <ted@brightsign.biz>
2017-03-07 12:03:54 -0800

    fixed exports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-export localPublisher from './publish/localPublisher';
-export sfnPublisher from './publish/sfnPublisher';
-export  lwsPublish  from './publish/lwsPublisher';
-export  PublishParams  from './publish/publicEntities/publishParams';
-export  generatePublishData  from './publish/publishDataGenerator';
-
+export localPublish from './publish/localPublisher';
+export sfnPublish from './publish/sfnPublisher';
+export lwsPublish  from './publish/lwsPublisher';
+export generatePublishData  from './publish/publishDataGenerator';
+export PublishParams  from './publish/publicEntities/publishParams';
+export PublishFirmware from './publish/publicEntities/publishFirmware';
+export PresentationToSchedule from './publish/publicEntities/presentationToSchedule';
+export ScheduledPresentation from './publish/publicEntities/scheduledPresentation';
