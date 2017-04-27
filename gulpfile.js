e5648797a24a2d1ca0a1843bc21a5414de443424
Ted Shaffer <ted@brightsign.biz>
2017-04-27 06:03:24 -0700

    don’t copy bundle in gulp as webpack now builds it in the right place.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,6 @@
 const gulp = require('gulp');
 
 gulp.task('default', () => {
-  gulp.src('build/bundle.js')
-    .pipe(gulp.dest('dist'));
   gulp.src('src/publish/publicEntities/*')
     .pipe(gulp.dest('dist'));
 });
